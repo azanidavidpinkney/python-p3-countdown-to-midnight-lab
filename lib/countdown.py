18362c67ab43bb36fb4d0cf57af85141bb5b050c
Azani David Pinkney <info@azani.org>
2023-12-10 12:52:27 -0800

    countdown lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time
+
+
+def countdown(i=5):
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+    print("HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(i=5):
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        time.sleep(1)
+        i -= 1
+    print("HAPPY NEW YEAR!")
